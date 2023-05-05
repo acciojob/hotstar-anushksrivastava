de88ebd6209c6aa1b9db81cfbe426c90fb8bae45
anushksrivastava <72188214+anushksrivastava@users.noreply.github.com>
2023-05-05 15:53:06 +0530

    Attempt 10

--- src/main/java/com/driver/model/Subscription.java
+++ src/main/java/com/driver/model/Subscription.java
@@ -2,13 +2,7 @@ package com.driver.model;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 
@@ -21,6 +15,7 @@ public class Subscription {
     private int id;
 
 
+    @Enumerated(EnumType.STRING)
     private SubscriptionType subscriptionType;
 
     private int noOfScreensSubscribed;
